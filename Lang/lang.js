7015d3db1ca844da04e2717512dae3f260e32d8e
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-01-28 18:12:09 +0300

    Minor edit.

--- Lang/lang.js
+++
@@ -1,9 +0,0 @@
-var lang = navigator.language || navigator.userLanguage;
-if (lang.starsWith("tr"))
-{
-    document.location.href = "/Documents/documents_tr.html";
-}
-else if (lang.starsWith("en"))
-{
-    document.location.href = "/Documents/documents.html";
-}
