ddc26d1db0158c4cecc460a9e61e3187e8d89428
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-02-23 15:38:41 +0300

    “YoutubeDownloader” has been removed from the list because it is not LinuxUsersLinuxMint software.

--- Lang/document.js
+++ Lang/document.js
@@ -3,20 +3,17 @@ window.onload = function()
     var lang = navigator.language;
     var pagetitle = document.getElementById("pagetitle");
     var paragraph = document.getElementById("paragraph");
-    var ytdown = document.getElementById("ytdown");
 
     if (lang.startsWith("tr"))
     {
         document.title = "Dokümanlar";
         pagetitle.innerHTML = "- LinuxUsersLinuxMint yazılımları için dokümanlar -";
         paragraph.innerHTML = "* Bu sayfada LinuxUsersLinuxMint yazılımlarının ne olduğu/nasıl kullanılacağı ile ilgili dokümanlar vardır.";
-        ytdown.innerHTML = "YoutubeDownloader (LinuxUsersLinuxMint tarafından katkıda bulunuldu , LinuxUsersLinuxMint yazılımı değildir.)";
     }
     else if (lang.startsWith("en"))
     {
         document.title = "Documents";
         pagetitle.innerHTML = "- Documentation for LinuxUsersLinuxMint software -";
         paragraph.innerHTML = "* This page contains documentation on what LinuxUsersLinuxMint software is/how to use it.";
-        ytdown.innerHTML = "YoutubeDownloader (Contributed by LinuxUsersLinuxMint , not LinuxUsersLinuxMint software)";
     }
 }
